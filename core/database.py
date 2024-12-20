96f0981240047b9c6355ae1ede678d5c2f8e70df
Pavel Pischalnikov <doessmth@gmail.com>
2024-12-20 23:43:02 +0500

    added db config, some minor changes

---
+++ core/database.py
@@ -0,0 +1,30 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+from core.settings import settings
+
+
+class Database:
+    def __init__(self):
+        self.engine = create_async_engine(
+            url=settings.db.url,
+            echo=True,
+        )
+        self.session_maker = async_sessionmaker(
+            bind=self.engine,
+            autoflush=False,
+            autocommit=False,
+            expire_on_commit=False,
+        )
+
+    async def get_async_session(self) -> AsyncGenerator[AsyncSession, None]:
+        async with self.session_maker() as session:
+            yield session
+
+
+db = Database()
--- core/models/base.py
+++ core/models/base.py
@@ -5,3 +5,6 @@ class Base(DeclarativeBase):
     __abstract__ = True
 
     id: Mapped[int] = mapped_column(primary_key=True)
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__}: id={self.id}>"
