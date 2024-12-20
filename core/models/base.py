7ec4b4ad4fb5227a2d683476b5bcf33692c8e6f3
Pavel Pischalnikov <doessmth@gmail.com>
2024-12-21 00:04:36 +0500

    added int ip mixin

--- core/models/base.py
+++ core/models/base.py
@@ -1,10 +1,6 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase
 
 
 class Base(DeclarativeBase):
-    __abstract__ = True
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"<{self.__class__.__name__}: id={self.id}>"
