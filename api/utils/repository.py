c7b3c3fdf16f41604bccfe78d42e69be6f83c669
Pavel Pischalnikov <doessmth@gmail.com>
2024-12-22 20:56:17 +0500

    added GetOneRepository

--- api/utils/repository.py
+++ api/utils/repository.py
@@ -1,5 +1,6 @@
-from typing import TYPE_CHECKING
+from typing import Any, TYPE_CHECKING
 
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 if TYPE_CHECKING:
@@ -7,17 +8,32 @@ if TYPE_CHECKING:
 
 
 class CreateRepo:
-    model: Base = None
+    model: "Base" = None
 
-    async def create(self, data_dict: dict, session: AsyncSession) -> Base:
+    async def create(self, data_dict: dict, session: AsyncSession) -> "Base":
         new_obj = self.model(**data_dict)
         session.add(new_obj)
         await session.commit()
         return new_obj
 
 
+class GetOneRepo:
+    model: "Base" = None
+
+    async def get_one(
+        self,
+        filters: dict[str, Any],
+        session: AsyncSession,
+    ) -> "Base":
+        stmt = select(self.model)
+        for key, value in filters.items():
+            if hasattr(self.model, key):
+                stmt = stmt.filter(getattr(self.model, key) == value)
+        return await session.scalar(stmt)
+
+
 class DeleteRepo:
-    model: Base = None
+    model: "Base" = None
 
     async def delete(self, session: AsyncSession) -> None:
         await session.delete(self.model)
