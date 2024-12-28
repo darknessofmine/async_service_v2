5f434d67529e32c96d64593eca1cc745e4731abd
Pavel Pischalnikov <doessmth@gmail.com>
2024-12-29 01:29:17 +0500

    removed extra lanes

--- background_tasks/celery.py
+++ background_tasks/celery.py
@@ -1,7 +1,5 @@
-from asgiref.sync import async_to_sync
 from celery import Celery
 
-from .notifications import mail
 from core.settings import settings
 
 
@@ -12,15 +10,3 @@ celery_app.conf.update(
     result_backend=settings.redis.url,
     broker_connection_retry_on_startup=True,
 )
-
-
-@celery_app.task()
-def send_reset_token(*args, **kwargs):
-    message = mail.create_reset_token_message(*args, **kwargs)
-    async_to_sync(mail.fast_mail.send_message)(message)
-
-
-@celery_app.task()
-def send_verification_url(*args, **kwargs):
-    message = mail.create_verification_url_message(*args, **kwargs)
-    async_to_sync(mail.fast_mail.send_message)(message)
