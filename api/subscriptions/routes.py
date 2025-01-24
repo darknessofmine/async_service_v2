8dfebfe16471b55da53dd0aa99c770e944f328cd
Pavel Pischalnikov <doessmth@gmail.com>
2025-01-24 21:21:30 +0500

    added model_return when unsub from chosen tier (setting sub_tier to null)

--- api/subscriptions/routes.py
+++ api/subscriptions/routes.py
@@ -30,13 +30,14 @@ async def subscribe(
 
 
 @router.post("/{username}/unsubscribe/{obj_id}",
+             response_model=SubscriptionResponse,
              status_code=status.HTTP_200_OK)
 async def unsubscribe(
     current_user: Annotated[User, Depends(AuthService.get_current_user)],
     sub_owner: Annotated[User, Depends(GetUserWithObjId("sub_tier"))],
     sub_service: Annotated[SubscriptionService, Depends(SubscriptionService)],
-) -> None:
-    await sub_service.unsubscribe_from_current_tier(
+) -> SubscriptionResponse:
+    return await sub_service.unsubscribe_from_current_tier(
         owner_id=sub_owner.id,
         client_id=current_user.id,
         sub_tier_id=sub_owner.sub_tiers[0].id,
